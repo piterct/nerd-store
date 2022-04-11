10dd5a30e1b788bb078e9759ef4fc80aa9419106
Michael Peter <michael_piterct@hotmail.com>
2022-04-11 00:31:05 -0300

    methods produto

--- NerdStore/src/NerdStore.Catalogo.Domain/Produto.cs
+++ NerdStore/src/NerdStore.Catalogo.Domain/Produto.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace NerdStore.Catalogo.Domain
 {
-    public class Produto: Entity, IAggregateRoot
+    public class Produto : Entity, IAggregateRoot
     {
         public Guid CategoriaId { get; private set; }
         public string Nome { get; private set; }
@@ -14,5 +14,48 @@ namespace NerdStore.Catalogo.Domain
         public string Imagem { get; private set; }
         public int QuantidadeEstoque { get; private set; }
         public Categoria Categoria { get; set; }
+
+        public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem)
+        {
+            CategoriaId = categoriaId;
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+        }
+
+        public void Ativar() => Ativo = true;
+
+        public void Desativar() => Ativo = false;
+
+        public void AlterarCategoria(Categoria categoria)
+        {
+            Categoria = categoria;
+            CategoriaId = categoria.Id;
+        }
+
+        public void AlterarDescricao(string descricao)
+        {
+            Descricao = descricao;
+        }
+
+        public void DebitarStoque(int quantidade)
+        {
+            if (quantidade < 0) quantidade *= -1;
+            QuantidadeEstoque -= quantidade;
+        }
+
+        public void ReporEstoque(int quantidade)
+        {
+            QuantidadeEstoque += quantidade;
+        }
+
+        public bool PossuiEstoque(int quantidade)
+        {
+            return QuantidadeEstoque >= quantidade;
+        }
     }
+
 }
