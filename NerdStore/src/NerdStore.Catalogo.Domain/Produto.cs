a63e9cf1ba24c88515973f5ad4a4b32413f6274f
Michael Peter <michael_piterct@hotmail.com>
2022-04-11 00:08:33 -0300

    produto entity

--- NerdStore/src/NerdStore.Catalogo.Domain/Produto.cs
+++ NerdStore/src/NerdStore.Catalogo.Domain/Produto.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using NerdStore.Core.DomainObjects;
+using System;
 
 namespace NerdStore.Catalogo.Domain
 {
-    public class Produto
+    public class Produto: Entity
     {
-        public Guid Id { get; set; }
     }
 }
