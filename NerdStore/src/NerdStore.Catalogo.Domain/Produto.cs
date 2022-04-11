ebf42ec1456a70fa562345d823407382561dad16
Michael Peter <michael_piterct@hotmail.com>
2022-04-11 00:17:17 -0300

    Produto

--- NerdStore/src/NerdStore.Catalogo.Domain/Produto.cs
+++ NerdStore/src/NerdStore.Catalogo.Domain/Produto.cs
@@ -1,8 +1,18 @@
 ﻿using NerdStore.Core.DomainObjects;
+using System;
 
 namespace NerdStore.Catalogo.Domain
 {
     public class Produto: Entity, IAggregateRoot
     {
+        public Guid CategoriaId { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public bool Ativo { get; private set; }
+        public decimal Valor { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public string Imagem { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
+        public Categoria Categoria { get; set; }
     }
 }
