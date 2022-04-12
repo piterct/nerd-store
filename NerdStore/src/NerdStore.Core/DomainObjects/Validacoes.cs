4292c1503025ed1fc969b3cb68ecaf8b40fa2fa7
Michael Peter <michael_piterct@hotmail.com>
2022-04-12 01:27:22 -0300

    ValidarCaracteres

--- NerdStore/src/NerdStore.Core/DomainObjects/Validacoes.cs
+++ NerdStore/src/NerdStore.Core/DomainObjects/Validacoes.cs
@@ -17,5 +17,14 @@
                 throw new DomainException(mensagem);
             }
         }
+
+        public static void ValidarCaracteres(string valor, int maximo, string mensagem)
+        {
+            var length = valor.Trim().Length;
+            if(length > maximo)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
     }
 }
