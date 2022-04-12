80099d6ed887006bbb5834cce5916335046ea0dd
Michael Peter <michael_piterct@hotmail.com>
2022-04-12 01:24:42 -0300

    Validações

---
+++ NerdStore/src/NerdStore.Core/DomainObjects/Validacoes.cs
@@ -0,0 +1,13 @@
+﻿namespace NerdStore.Core.DomainObjects
+{
+    public class Validacoes
+    {
+        public static void ValidarSeIgual(object object1, object object2, string mensagem)
+        {
+            if (!object1.Equals(object2))
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+    }
+}
