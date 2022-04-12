8a46cb577dcb2046c558d7ce65dc6b18c1eb58b6
Michael Peter <michael_piterct@hotmail.com>
2022-04-12 01:21:30 -0300

    contructions domain exception

--- NerdStore/src/NerdStore.Core/DomainObjects/DomainException.cs
+++ NerdStore/src/NerdStore.Core/DomainObjects/DomainException.cs
@@ -4,5 +4,13 @@ namespace NerdStore.Core.DomainObjects
 {
     public class DomainException : Exception
     {
+        public DomainException()
+        { }
+
+        public DomainException(string message) : base(message)
+        { }
+
+        public DomainException(string message, Exception innerException) : base(message, innerException)
+        { }
     }
 }
