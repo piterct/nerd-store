3ce88acb8889318da2fc7748ed3b3de6838d4dad
Michael Peter <michael_piterct@hotmail.com>
2022-04-12 01:46:26 -0300

    Validar categoria

--- NerdStore/src/NerdStore.Catalogo.Domain/Categoria.cs
+++ NerdStore/src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -11,11 +11,19 @@ namespace NerdStore.Catalogo.Domain
         {
             Nome = nome;
             Codigo = codigo;
+
+            Validar();
         }
 
         public override string ToString()
         {
             return $"{Nome} - {Codigo}";
+        }
+
+        public void Validar()
+        {
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome da categoria não pode estar vazio");
+            Validacoes.ValidarSeIgual(Codigo, 0, "O campo Codigo não pode ser 0");
         }
     }
 }
