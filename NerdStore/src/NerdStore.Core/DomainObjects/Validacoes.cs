3d01b3a445395bb6f54ff3f0cbfd0a04885e5308
Michael Peter <michael_piterct@hotmail.com>
2022-04-13 05:17:41 -0300

    fix validate object equals

--- NerdStore/src/NerdStore.Core/DomainObjects/Validacoes.cs
+++ NerdStore/src/NerdStore.Core/DomainObjects/Validacoes.cs
@@ -6,7 +6,7 @@ namespace NerdStore.Core.DomainObjects
     {
         public static void ValidarSeIgual(object object1, object object2, string mensagem)
         {
-            if (!object1.Equals(object2))
+            if (object1.Equals(object2))
             {
                 throw new DomainException(mensagem);
             }
@@ -14,7 +14,7 @@ namespace NerdStore.Core.DomainObjects
 
         public static void ValidarSeDiferente(object object1, object object2, string mensagem)
         {
-            if (object1.Equals(object2))
+            if (!object1.Equals(object2))
             {
                 throw new DomainException(mensagem);
             }
