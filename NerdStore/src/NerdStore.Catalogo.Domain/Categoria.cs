1431483af896295c3de9f800de1e65eaba5a35e4
Michael Peter <michael_piterct@hotmail.com>
2022-04-14 02:43:48 -0300

    //EF Relation Produtos

--- NerdStore/src/NerdStore.Catalogo.Domain/Categoria.cs
+++ NerdStore/src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -1,11 +1,17 @@
 ﻿using NerdStore.Core.DomainObjects;
+using System.Collections.Generic;
 
 namespace NerdStore.Catalogo.Domain
 {
-    public class Categoria: Entity
+    public class Categoria : Entity
     {
         public string Nome { get; private set; }
         public int Codigo { get; private set; }
+
+        //EF Relation
+        public ICollection<Produto> Produtos { get; set; }
+
+        protected Categoria() { }
 
         public Categoria(string nome, int codigo)
         {
