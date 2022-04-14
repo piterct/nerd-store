baaf1dbfd293d7c9591b8135d861019ae897f182
Michael Peter <michael_piterct@hotmail.com>
2022-04-14 02:23:29 -0300

    Catalogo Data

--- NerdStore/src/NerdStore.Catalogo.Data/CatalogoContext.cs
+++ NerdStore/src/NerdStore.Catalogo.Data/CatalogoContext.cs
@@ -1,6 +1,30 @@
-﻿namespace NerdStore.Catalogo.Data
+﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Data
 {
-    public class CatalogoContext
+    public class CatalogoContext : DbContext, IUnitOfWork
     {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options)
+           : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+              e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.Relational().ColumnType = "varchar(100)";
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public Task<bool> Commit()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
