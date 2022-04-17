53c1af211f4694c7ea98daa8b4b597c38b5cb712
Michael Peter <michael_piterct@hotmail.com>
2022-04-17 00:38:12 -0300

    Message

---
+++ NerdStore/src/NerdStore.Core/Messages/Message.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NerdStore.Core.Messages
+{
+    public abstract class Message
+    {
+        public string MessageType { get; protected set; }
+        public Guid AggregateId { get; protected set; }
+    }
+}
