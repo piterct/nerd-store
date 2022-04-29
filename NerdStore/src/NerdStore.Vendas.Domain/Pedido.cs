065b515b2dc79b264b4b08421dff99a9bbe0f284
Michael Peter <michael_piterct@hotmail.com>
2022-04-29 02:26:42 -0300

    Pedido

--- NerdStore/src/NerdStore.Vendas.Domain/Pedido.cs
+++ NerdStore/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -1,8 +1,24 @@
 ﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
 
 namespace NerdStore.Vendas.Domain
 {
-    public class Pedido: Entity, IAggregateRoot
+    public class Pedido : Entity, IAggregateRoot
     {
+        public int Codigo { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public Guid? VoucherId { get; private set; }
+        public bool VoucherUtilizado { get; private set; }
+        public decimal Desconto { get; private set; }
+        public decimal ValorTotal { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public PedidoStatus PedidoStatus { get; private set; }
+
+        private readonly List<PedidoItem> _pedidoItems;
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+
+        // EF Rel.
+        public virtual Voucher Voucher { get; set; }
     }
 }
