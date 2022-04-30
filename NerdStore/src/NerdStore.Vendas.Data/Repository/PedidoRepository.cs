c4904e4864f0dacec2c5154efee8fc5ce7a90d73
Michael Peter <michael_piterct@hotmail.com>
2022-04-30 15:20:26 -0300

    Catalogo contexto

---
+++ NerdStore/src/NerdStore.Vendas.Data/Repository/PedidoRepository.cs
@@ -0,0 +1,73 @@
+﻿using NerdStore.Core.Data;
+using NerdStore.Vendas.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Data.Repository
+{
+    public class PedidoRepository : IPedidoRepository
+    {
+        public IUnitOfWork UnitOfWork => throw new NotImplementedException();
+
+        public void Adicionar(Pedido pedido)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AdicionarItem(PedidoItem pedidoItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Pedido pedido)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AtualizarItem(PedidoItem pedidoItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PedidoItem> ObterItemPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PedidoItem> ObterItemPorPedido(Guid pedidoId, Guid produtoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Pedido>> ObterListaPorClienteId(Guid clienteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Pedido> ObterPedidoRascunhoPorClienteId(Guid clienteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Pedido> ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Voucher> ObterVoucherPorCodigo(string codigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoverItem(PedidoItem pedidoItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
