f25c088ab31cba655350248c676bd443feb5fbc1
Michael Peter <michael_piterct@hotmail.com>
2022-04-30 12:43:37 -0300

    CarrinhoController

---
+++ NerdStore/src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Vendas.Application.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.MVC.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        private readonly IProdutoAppService _produtoAppService;
+
+        public CarrinhoController(IProdutoAppService produtoAppService)
+        {
+            _produtoAppService = produtoAppService;
+        }
+
+        [HttpPost]
+        [Route("meu-carrinho")]
+        public async Task<IActionResult> AdicionarItem(Guid id, int quantidade)
+        {
+            var produto = await _produtoAppService.ObterPorId(id);
+            if (produto == null) return BadRequest();
+
+            if (produto.QuantidadeEstoque < quantidade)
+            {
+                TempData["Erro"] = "Produto com estoque insuficiente";
+                return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
+            }
+
+            var command = new AdicionarItemPedidoCommand(ClienteId, produto.Id, produto.Nome, quantidade, produto.Valor);
+
+
+
+            TempData["Erros"] = "Produto Indisponivel";
+            return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
+        }
+    }
+}
