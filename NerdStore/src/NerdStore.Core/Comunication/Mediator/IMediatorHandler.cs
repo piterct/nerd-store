f962cf96029d4acaad5f177bc633595faf42feb3
Michael Peter <michael_piterct@hotmail.com>
2022-05-02 07:19:56 -0300

    PublicarNotificacao

--- NerdStore/src/NerdStore.Core/Comunication/Mediator/IMediatorHandler.cs
+++ NerdStore/src/NerdStore.Core/Comunication/Mediator/IMediatorHandler.cs
@@ -1,4 +1,5 @@
 ﻿using NerdStore.Core.Messages;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
 using System.Threading.Tasks;
 
 namespace NerdStore.Core.Comunication.Mediator
@@ -7,5 +8,6 @@ namespace NerdStore.Core.Comunication.Mediator
     {
         Task PublicarEvento<T>(T evento) where T: Event;
         Task<bool> EnviarComando<T>(T evento) where T : Command;
+        Task PublicarNotificacao<T>(T notificacao) where T : DomainNotification;
     }
 }
--- NerdStore/src/NerdStore.Core/Comunication/Mediator/MediatorHandler.cs
+++ NerdStore/src/NerdStore.Core/Comunication/Mediator/MediatorHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using NerdStore.Core.Messages;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
 using System.Threading.Tasks;
 
 namespace NerdStore.Core.Comunication.Mediator
@@ -21,6 +22,11 @@ namespace NerdStore.Core.Comunication.Mediator
         public async Task PublicarEvento<T>(T evento) where T : Event
         {
             await _mediator.Publish(evento);
+        }
+
+        public async Task PublicarNotificacao<T>(T notificacao) where T : DomainNotification
+        {
+            await _mediator.Publish(notificacao);
         }
     }
 
