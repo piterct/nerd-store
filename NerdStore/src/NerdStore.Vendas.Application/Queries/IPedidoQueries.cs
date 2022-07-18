2d7fc3b72c4452ab5821f33b8c193f9f4b52fe33
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-07-17 23:10:12 -0300

    IPedidoQueries (ObterCarrinhoCliente and ObterPedidosCliente)

--- NerdStore/src/NerdStore.Vendas.Application/Queries/IPedidoQueries.cs
+++ NerdStore/src/NerdStore.Vendas.Application/Queries/IPedidoQueries.cs
@@ -1,6 +1,13 @@
-﻿namespace NerdStore.Vendas.Application.Queries
+﻿using NerdStore.Vendas.Application.Queries.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Application.Queries
 {
     public interface  IPedidoQueries
     {
+        Task<CarrinhoViewModel> ObterCarrinhoCliente(Guid clienteId);
+        Task<IEnumerable<PedidoViewModel>> ObterPedidosCliente(Guid clienteId);
     }
 }
