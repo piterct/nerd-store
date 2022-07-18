6180414696a15c7f4c5fc707ac1deb1a48b00608
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-07-17 23:18:01 -0300

    ObterCarrinhoCliente query

--- NerdStore/src/NerdStore.Vendas.Application/Queries/PedidoQueries.cs
+++ NerdStore/src/NerdStore.Vendas.Application/Queries/PedidoQueries.cs
@@ -1,4 +1,5 @@
 ﻿using NerdStore.Vendas.Application.Queries.ViewModels;
+using NerdStore.Vendas.Domain;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -7,12 +8,46 @@ namespace NerdStore.Vendas.Application.Queries
 {
     public class PedidoQueries : IPedidoQueries
     {
-        public Task<CarrinhoViewModel> ObterCarrinhoCliente(Guid clienteId)
+        private readonly IPedidoRepository _pedidoRepository;
+        public PedidoQueries(IPedidoRepository pedidoRepository)
         {
-            throw new NotImplementedException();
+            _pedidoRepository = pedidoRepository;
+        }
+        public async Task<CarrinhoViewModel> ObterCarrinhoCliente(Guid clienteId)
+        {
+            var pedido = await _pedidoRepository.ObterPedidoRascunhoPorClienteId(clienteId);
+            if (pedido == null) return null;
+
+            var carrinho = new CarrinhoViewModel
+            {
+                ClienteId = pedido.ClienteId,
+                ValorTotal = pedido.ValorTotal,
+                PedidoId = pedido.Id,
+                ValorDesconto = pedido.Desconto,
+                SubTotal = pedido.Desconto + pedido.ValorTotal
+            };
+
+            if (pedido.VoucherId != null)
+            {
+                carrinho.VoucherCodigo = pedido.Voucher.Codigo;
+            }
+
+            foreach (var item in pedido.PedidoItems)
+            {
+                carrinho.Itens.Add(new CarrinhoItemViewModel
+                {
+                    ProdutoId = item.ProdutoId,
+                    ProdutoNome = item.ProdutoNome,
+                    Quantidade = item.Quantidade,
+                    ValorUnitario = item.ValorUnitario,
+                    ValorTotal = item.ValorUnitario * item.Quantidade
+                });
+            }
+
+            return carrinho;
         }
 
-        public Task<IEnumerable<PedidoViewModel>> ObterPedidosCliente(Guid clienteId)
+        public async Task<IEnumerable<PedidoViewModel>> ObterPedidosCliente(Guid clienteId)
         {
             throw new NotImplementedException();
         }
