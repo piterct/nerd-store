33d136d85149eeccf178755ef12eb570a34fee49
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-07-18 00:17:15 -0300

    Cart Component

---
+++ NerdStore/src/NerdStore.WebApp.MVC/Extensions/CartViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdStore.Vendas.Application.Queries;
+using System;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.MVC.Extensions
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly IPedidoQueries _pedidoQueries;
+
+        // TODO: Obter cliente logado
+        protected Guid ClienteId = Guid.Parse("04A00A43-8FC6-4F12-A0DA-0AD4836F3AE6");
+
+
+        public CartViewComponent(IPedidoQueries pedidoQueries)
+        {
+            _pedidoQueries = pedidoQueries;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var carrinho = await _pedidoQueries.ObterCarrinhoCliente(ClienteId);
+            var itens = carrinho?.Itens.Count ?? 0;
+
+            return View(itens);
+        }
+    }
+}
