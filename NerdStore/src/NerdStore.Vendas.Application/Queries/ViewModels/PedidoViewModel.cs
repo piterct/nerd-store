21dd0eae6047cf984dd96308390a4b51d8186df0
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-07-17 23:20:58 -0300

    ObterPedidosCliente query

--- NerdStore/src/NerdStore.Vendas.Application/Queries/ViewModels/PedidoViewModel.cs
+++ NerdStore/src/NerdStore.Vendas.Application/Queries/ViewModels/PedidoViewModel.cs
@@ -4,8 +4,9 @@ namespace NerdStore.Vendas.Application.Queries.ViewModels
 {
     public class PedidoViewModel
     {
+        public Guid Id { get; set; }
         public int Codigo { get; set; }
-        public decimal ValorPedido { get; set; }
+        public decimal ValorTotal { get; set; }
         public DateTime DataCadastro { get; set; }
         public int PedidoStatus { get; set; }
     }
