ed242a81eeba4ca554d8e4f685b66b37cb496932
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-08-07 19:33:21 -0300

    RemoverItemPedidoCommand

---
+++ NerdStore/src/NerdStore.Vendas.Application/Commands/RemoverItemPedidoCommand.cs
@@ -0,0 +1,11 @@
+﻿using NerdStore.Core.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Vendas.Application.Commands
+{
+    public class RemoverItemPedidoCommand : Command
+    {
+    }
+}
