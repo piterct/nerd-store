d3420b829a5d94927f58f623bbf74ebb428f64d7
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-08-07 19:48:15 -0300

    AplicarVoucherPedidoValidation

--- NerdStore/src/NerdStore.Vendas.Application/Commands/AplicarVoucherPedidoCommand.cs
+++ NerdStore/src/NerdStore.Vendas.Application/Commands/AplicarVoucherPedidoCommand.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using FluentValidation;
+using NerdStore.Core.Messages;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace NerdStore.Vendas.Application.Commands
 {
-    public class AplicarVoucherPedidoCommand
+    public class AplicarVoucherPedidoCommand : Command
     {
+        public AplicarVoucherPedidoCommand(Guid clienteId, Guid pedidoId, string codigoVoucher)
+        {
+            ClienteId = clienteId;
+            PedidoId = pedidoId;
+            CodigoVoucher = codigoVoucher;
+        }
+
+        public Guid ClienteId { get; private set; }
+        public Guid PedidoId { get; private set; }
+        public string CodigoVoucher { get; private set; }
+    }
+
+    public class AplicarVoucherPedidoValidation : AbstractValidator<AplicarVoucherPedidoCommand>
+    {
+        public AplicarVoucherPedidoValidation()
+        {
+            RuleFor(c => c.ClienteId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do cliente inválido");
+
+            RuleFor(c => c.PedidoId)
+               .NotEqual(Guid.Empty)
+               .WithMessage("Id do pedido inválido");
+
+            RuleFor(c => c.CodigoVoucher)
+                .NotEmpty()
+                .WithMessage("O código do voucher não pode ser vazio");
+        }
     }
 }
