54ce43ce13cc883d1c0e75622725383df4f9a113
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-08-07 19:43:00 -0300

    RemoverItemPedidoValidation

--- NerdStore/src/NerdStore.Vendas.Application/Commands/RemoverItemPedidoCommand.cs
+++ NerdStore/src/NerdStore.Vendas.Application/Commands/RemoverItemPedidoCommand.cs
@@ -1,4 +1,5 @@
-﻿using NerdStore.Core.Messages;
+﻿using FluentValidation;
+using NerdStore.Core.Messages;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,33 @@ namespace NerdStore.Vendas.Application.Commands
 {
     public class RemoverItemPedidoCommand : Command
     {
+        public RemoverItemPedidoCommand(Guid clienteId, Guid produtoId)
+        {
+            ClienteId = clienteId;
+            ProdutoId = produtoId;
+        }
+
+        public Guid ClienteId { get; private set; }
+        public Guid ProdutoId { get; private set; }
+
+        public override bool EhValido()
+        {
+            ValidationResult = new RemoverItemPedidoValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+
+    public class RemoverItemPedidoValidation : AbstractValidator<RemoverItemPedidoCommand>
+    {
+        public RemoverItemPedidoValidation()
+        {
+            RuleFor(c => c.ClienteId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do cliente inválido");
+
+            RuleFor(c => c.ProdutoId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do produto inválido");
+        }
     }
 }
