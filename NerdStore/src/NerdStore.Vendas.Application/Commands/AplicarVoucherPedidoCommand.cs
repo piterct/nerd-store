f57988a9c373d05ea0eff8458355408b155c1354
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-08-07 19:34:03 -0300

    AplicarVoucherPedidoCommand

---
+++ NerdStore/src/NerdStore.Vendas.Application/Commands/AplicarVoucherPedidoCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Vendas.Application.Commands
+{
+    public class AplicarVoucherPedidoCommand
+    {
+    }
+}
