4e86cef47486b558275eea04dc7e3d8df47d36fc
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-08-09 23:42:59 -0300

    dependency injection

--- NerdStore/src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ NerdStore/src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -41,6 +41,9 @@ namespace NerdStore.WebApp.MVC.Setup
             services.AddScoped<VendasContext>();
 
             services.AddScoped<IRequestHandler<AdicionarItemPedidoCommand, bool>, PedidoCommandHandler>();
+            services.AddScoped<IRequestHandler<AtualizarItemPedidoCommand, bool>, PedidoCommandHandler>();
+            services.AddScoped<IRequestHandler<RemoverItemPedidoCommand, bool>, PedidoCommandHandler>();
+            services.AddScoped<IRequestHandler<AplicarVoucherPedidoCommand, bool>, PedidoCommandHandler>();
 
             // Notifications
             services.AddScoped<INotificationHandler<PedidoRascunhoIniciadoEvent>, PedidoEventHandler>();
