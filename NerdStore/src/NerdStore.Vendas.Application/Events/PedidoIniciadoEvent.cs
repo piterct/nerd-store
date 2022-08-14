74604a40a260f97e1c78025e44ab76fb14f04ad3
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-08-13 23:27:18 -0300

    PedidoIniciadoEvent and ListaProdutosPedido

---
+++ NerdStore/src/NerdStore.Vendas.Application/Events/PedidoIniciadoEvent.cs
@@ -0,0 +1,33 @@
+﻿using NerdStore.Core.DomainObjects.DTO;
+using NerdStore.Core.Messages;
+using System;
+
+namespace NerdStore.Vendas.Application.Events
+{
+    public class PedidoIniciadoEvent : Event
+    {
+
+        public Guid PedidoId { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public decimal Total { get; private set; }
+        public ListaProdutosPedido ProdutosPedido { get; private set; }
+        public string NomeCartao { get; private set; }
+        public string NumeroCartao { get; private set; }
+        public string ExpiracaoCartao { get; private set; }
+        public string CvvCartao { get; private set; }
+
+        public PedidoIniciadoEvent(Guid pedidoId, Guid clienteId, ListaProdutosPedido itens, decimal total, string nomeCartao, string numeroCartao, string expiracaoCartao, string cvvCartao)
+        {
+            AggregateId = pedidoId;
+            PedidoId = pedidoId;
+            ClienteId = clienteId;
+            ProdutosPedido = itens;
+            Total = total;
+            NomeCartao = nomeCartao;
+            NumeroCartao = numeroCartao;
+            ExpiracaoCartao = expiracaoCartao;
+            CvvCartao = cvvCartao;
+        }
+
+    }
+}
