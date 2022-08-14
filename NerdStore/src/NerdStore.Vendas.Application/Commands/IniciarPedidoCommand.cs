505825edeadba349f4d8bff34af6bbfebbaca49e
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-08-13 22:30:49 -0300

    IniciarPedidoCommand

---
+++ NerdStore/src/NerdStore.Vendas.Application/Commands/IniciarPedidoCommand.cs
@@ -0,0 +1,29 @@
+﻿using NerdStore.Core.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Vendas.Application.Commands
+{
+    public  class IniciarPedidoCommand : Command
+    {
+        public Guid PedidoId { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public decimal Total { get; private set; }
+        public string NomeCartao { get; private set; }
+        public string NumeroCartao { get; private set; }
+        public string ExpiracaoCartao { get; set; }
+        public string CvvCartao { get; set; }
+
+        public IniciarPedidoCommand(Guid pedidoId, Guid clienteId, decimal total, string nomeCartao, string numeroCartao, string expiracaoCartao, string cvvCartao)
+        {
+            PedidoId = pedidoId;
+            ClienteId = clienteId;
+            Total = total;
+            NomeCartao = nomeCartao;
+            NumeroCartao = numeroCartao;
+            ExpiracaoCartao = expiracaoCartao;
+            CvvCartao = cvvCartao;
+        }
+    }
+}
