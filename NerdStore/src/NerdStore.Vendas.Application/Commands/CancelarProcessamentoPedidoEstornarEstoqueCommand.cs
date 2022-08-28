a654313c3ad0be6affa97a16d01f581f1a0d97a1
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-08-27 21:49:22 -0300

    CancelarProcessamentoPedidoEstornarEstoqueCommand

---
+++ NerdStore/src/NerdStore.Vendas.Application/Commands/CancelarProcessamentoPedidoEstornarEstoqueCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NerdStore.Vendas.Application.Commands
+{
+    public class CancelarProcessamentoPedidoEstornarEstoqueCommand
+    {
+        public CancelarProcessamentoPedidoEstornarEstoqueCommand(Guid pedidoId, Guid clienteId)
+        {
+            PedidoId = pedidoId;
+            ClienteId = clienteId;
+        }
+
+        public Guid PedidoId { get; set; }
+        public Guid ClienteId { get; set; }
+    }
+}
