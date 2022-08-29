706ea48bd6644fc4be22f21b529d6d4d2744e0dc
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-08-29 00:40:56 -0300

    PedidoFinalizadoEvent

---
+++ NerdStore/src/NerdStore.Vendas.Application/Events/PedidoFinalizadoEvent.cs
@@ -0,0 +1,16 @@
+﻿using NerdStore.Core.Messages;
+using System;
+
+namespace NerdStore.Vendas.Application.Events
+{
+    public  class PedidoFinalizadoEvent : Event
+    {
+        public PedidoFinalizadoEvent(Guid pedidoId)
+        {
+            PedidoId = pedidoId;
+        }
+
+        public Guid PedidoId { get; set; }
+
+    }
+}
