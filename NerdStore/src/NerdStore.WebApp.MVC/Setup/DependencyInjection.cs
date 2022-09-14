bc8f567840e69a46ec5df5a177957e7436627bfb
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-09-13 22:46:39 -0300

    dependency injection

--- NerdStore/src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ NerdStore/src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using EventSourcing;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using NerdStore.Catalogo.Application.Services;
 using NerdStore.Catalogo.Data;
@@ -31,6 +32,9 @@ namespace NerdStore.WebApp.MVC.Setup
 
             //Notifications
             services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
+
+            // Event Sourcing
+            services.AddSingleton<IEventStoreService, EventStoreService>();
 
             // Catalogo
             services.AddScoped<IProdutoRepository, ProdutoRepository>();
