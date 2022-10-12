212076183dc7a1e1a9a78703d1d489724fae7a84
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-10-11 23:28:08 -0300

    EventSourcingRepository

--- NerdStore/src/EventSourcing/EventSourcingRepository.cs
+++ NerdStore/src/EventSourcing/EventSourcingRepository.cs
@@ -1,8 +1,28 @@
 ﻿using NerdStore.Core.Data.EventSourcing;
+using NerdStore.Core.Messages;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace EventSourcing
 {
-    public  class EventSourcingRepository: IEventSourcingRepository
+    public class EventSourcingRepository : IEventSourcingRepository
     {
+        private readonly IEventStoreService _eventStoreService;
+
+        public EventSourcingRepository(IEventStoreService eventStoreService)
+        {
+            _eventStoreService = eventStoreService;
+        }
+        public Task SalvarEvento<TEvent>(TEvent evento) where TEvent : Event
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IEnumerable<StoredEvent>> ObterEventos(Guid aggregateId)
+        {
+            throw new NotImplementedException();
+        }
+
+       
     }
 }
