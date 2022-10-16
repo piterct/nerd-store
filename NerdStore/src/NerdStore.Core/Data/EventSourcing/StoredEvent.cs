ee275713a5acc1fc5b560fb1d7fc4149c75c5fac
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-10-16 20:59:32 -0300

    StoredEvent

--- NerdStore/src/NerdStore.Core/Data/EventSourcing/StoredEvent.cs
+++ NerdStore/src/NerdStore.Core/Data/EventSourcing/StoredEvent.cs
@@ -4,6 +4,13 @@ namespace NerdStore.Core.Data.EventSourcing
 {
     public class StoredEvent
     {
+        public StoredEvent(Guid id, string tipo, DateTime dataOcorrencia, string dados)
+        {
+            Id = id;
+            Tipo = tipo;
+            DataOcorrencia = dataOcorrencia;
+            Dados = dados;
+        }
 
         public Guid Id { get; private set; }
         public string Tipo { get; private set; }
