7606df6b98fdf1ecc131317cfda02166eb5a036d
Michael Peter <42197306+piterct@users.noreply.github.com>
2022-11-18 21:30:55 -0300

    Eventos Controller

--- NerdStore/src/NerdStore.WebApp.MVC/Controllers/EventosController.cs
+++ NerdStore/src/NerdStore.WebApp.MVC/Controllers/EventosController.cs
@@ -1,6 +1,29 @@
-﻿namespace NerdStore.WebApp.MVC.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdStore.Core.Data.EventSourcing;
+using System.Threading.Tasks;
+using System;
+using MediatR;
+using NerdStore.Core.Comunication.Mediator;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
+
+namespace NerdStore.WebApp.MVC.Controllers
 {
-    public class EventosController
+    public class EventosController : ControllerBase
     {
+        private readonly IEventSourcingRepository _eventSourcingRepository;
+
+        public EventosController(IEventSourcingRepository eventSourcingRepository,
+             INotificationHandler<DomainNotification> notifications,
+            IMediatorHandler mediatorHandler) : base(notifications, mediatorHandler)
+        {
+            _eventSourcingRepository = eventSourcingRepository;
+        }
+
+        [HttpGet("eventos/{id:guid}")]
+        public async Task<IActionResult> Index(Guid id)
+        {
+            var eventos = await _eventSourcingRepository.ObterEventos(id);
+            return View(eventos);
+        }
     }
 }
